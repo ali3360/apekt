09ff517584f2b57aba7db2a600ba2bc5da42712e
ali3360 <ALIPARTOVE78@GMAIL.COM>
2017-04-24 16:15:13 +0430

    Update creator.py

--- creator.py
+++ creator.py
@@ -3,14 +3,14 @@
  +import redis
  +import os
  +redis = redis.Redis('localhost')
- +id = input("Enter Tabchi ID (1,2,3,4,5,...) : ")
+ +id = input("Enter jarchi ID (1,2,3,4,5,...) : ")
  +sudo = input("Enter Full Sudo ID : ")
  +source = os.popen("cat base.lua").read()
  +launcher = """while true; do
- +  ./telegram-cli-1222 -p jarchi-{} -s tabchi-{}.lua
+ +  ./telegram-cli-1222 -p jarchi-{} -s jarchi-{}.lua
  +done""".format(id,id)
  +source2 = source.replace("jarchi-ID",str(id))
- +newsource = open("tabchi-{}.lua".format(id),"w")
+ +newsource = open("jarchi-{}.lua".format(id),"w")
  +newsource.write(source2)
  +newsource.close()
  +newlauncher = open("jarchi-{}.sh".format(id),"w")
